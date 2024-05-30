aa13ee53dec70efc4ce9fc9252f2bc10bf6fc023
Ulf Luecking <ulf.luecking@gmail.com>
2024-05-30 13:13:27 +0200

    ch08

--- Chapter08/rating/cmd/main.go
+++ Chapter08/rating/cmd/main.go
@@ -49,7 +49,12 @@ func main() {
 			time.Sleep(1 * time.Second)
 		}
 	}()
-	defer registry.Deregister(ctx, instanceID, serviceName)
+	defer func() {
+		err := registry.Deregister(ctx, instanceID, serviceName)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 	repo := memory.New()
 	ctrl := rating.New(repo, nil)
 	h := grpchandler.New(ctrl)
